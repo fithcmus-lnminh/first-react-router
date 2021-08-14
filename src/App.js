ddf0a99b5719e773fc2d0a32f25987ea0a487166
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-08-14 19:21:18 +0700

    using Switch and exact (exactly link) for configuring routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
 import ProductDetails from "./pages/ProductDetails";
 import Products from "./pages/Products";
@@ -9,15 +9,18 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/products/:productId">
-          <ProductDetails />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            {/*when path exactly "/products" -> display product component*/}
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetails />
+          </Route>
+        </Switch>
       </main>
     </div>
   );

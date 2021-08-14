b4fe0a4d99d0b698052b846aceff6b752016a3a7
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-08-14 13:49:47 +0700

    using NavLink instead of Link to style for the attribute

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,16 +1,21 @@
+import classes from "./MainHeader.module.css";
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const MainHeader = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <nav>
         <ul>
           <li>
-            <Link to="/welcome">Welcome</Link>
+            <NavLink activeClassName={classes.active} to="/welcome">
+              Welcome
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink activeClassName={classes.active} to="/products">
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
